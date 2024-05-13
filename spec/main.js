db65281aef05d448ad3c9dbae43eaa67b0fc3e99
OshkarVTec <a01275287@tec.mx>
2024-05-13 11:33:39 -0600

    Prueba 2

--- spec/main.js
+++ spec/main.js
@@ -4,3 +4,24 @@ describe("Test Example", () => {
 		expect(true).toBe(true);
 	});
 });
+
+function insertDashes(str) {
+	// write code here
+}
+/**
+ * Test Suite
+ */
+describe("insertDashes()", () => {
+	it("insert dashes in between chars", () => {
+		// arrange
+		const value = "aba caba";
+
+		// act
+		const result = insertDashes(value);
+		// log
+		console.log("result: ", result);
+
+		// assert
+		expect(result).toBe(undefined);
+	});
+});
